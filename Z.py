cdc03abf9bb226bab6a5de6445856ca2f7e9e4a1
Rdxcj <qoefhsbfnx@gmail.com>
2024-08-31 12:29:18 +0530

    1

--- Z.py
+++ Z.py
@@ -173,17 +173,17 @@ json_data = {
             ],
         },
     },
-    'videoId': 'Vn-VgqSTx_U',
+    'videoId': 'YNWHR6lMk1c',
     'playbackContext': {
         'contentPlaybackContext': {
-            'currentUrl': '/watch?v=Vn-VgqSTx_U',
+            'currentUrl': '/watch?v=YNWHR6lMk1c',
             'vis': 0,
             'splay': False,
             'autoCaptionsDefaultOn': False,
             'autonavState': 'STATE_NONE',
             'html5Preference': 'HTML5_PREF_WANTS',
             'signatureTimestamp': 19949,
-            'referer': 'https://www.youtube.com/watch?v=Vn-VgqSTx_U',
+            'referer': 'https://www.youtube.com/watch?v=YNWHR6lMk1c',
             'lactMilliseconds': '-1',
             'watchAmbientModeContext': {
                 'hasShownAmbientMode': True,
@@ -222,7 +222,7 @@ print(response.text)
 
 
 #short -ß
-os.system(f"ffmpeg -http_persistent 0 -re -ss 05:59:00 -i '{pr}' -max_muxing_queue_size 9999 -threads 4 -vf \"format=yuv420p,drawtext=fontfile=_.ttf:text='F B':fontcolor=white:fontsize=130:line_spacing=32:bordercolor=black:borderw=10:box=0:boxcolor=black@0.1:boxborderw=80:x=w-tw-20:y=h-th-20,transpose=1\" -c:v libx264 -g 48 -b:v 9000k -c:a aac -preset ultrafast -tune zerolatency -f flv rtmp://a.rtmp.youtube.com/live2/q97f-38zg-czwr-qwah-b2wy")
+os.system(f"ffmpeg -http_persistent 0 -re -ss 05:58:00 -i '{pr}' -max_muxing_queue_size 9999 -threads 4 -vf \"format=yuv420p,drawtext=fontfile=_.ttf:text='F B':fontcolor=white:fontsize=130:line_spacing=32:bordercolor=black:borderw=10:box=0:boxcolor=black@0.1:boxborderw=80:x=w-tw-20:y=h-th-20\" -c:v libx264 -g 48 -b:v 9000k -c:a aac -preset ultrafast -tune zerolatency -f flv rtmp://a.rtmp.youtube.com/live2/sz1s-guq5-m83v-t1ss-5p89")
 
 
 #os.system(f"ffmpeg -http_persistent 0 -ss 10:00:00 -re -i '{pr}' -threads 4 -vcodec copy -c:a copy -preset ultrafast -tune zerolatency -f flv rtmp://a.rtmp.youtube.com/live2/u151-yfj5-6g19-c006-a0js")
